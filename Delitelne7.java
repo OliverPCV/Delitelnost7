447fbd32eb0a7e3df730d58f22c191e2da90395e
OliverPCV <oliver.cavallo@gmail.com>
2020-09-07 11:17:15 +0200

    hotovo

--- Delitelne7.java
+++ Delitelne7.java
@@ -4,6 +4,7 @@ class Delitelne7 {
     public static void main(String args[]){
 
         int number;
+        int prvocisla = 0;
 
         Scanner sc = new Scanner(System.in);
         number = sc.nextInt();
@@ -16,6 +17,18 @@ class Delitelne7 {
 
         }
 
-
+        for(int i = 2; i <= 100; i++){
+            for(int j = 2; j <= 100; j++){
+                if(i % j == 0){
+                    prvocisla += 1;
+                }
+            }
+        
+           if(prvocisla < 2){
+               System.out.println(i);
+           }
+           prvocisla = 0;
+        }
+         
     }
 }
